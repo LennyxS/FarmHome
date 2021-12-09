6695af1cc974f35955c4742ac9f90d1729cfb12b
LennyxS <lennyxsebastian11@gmail.com>
2021-12-08 19:27:18 -0500

    Arreglos en la App de frontend

--- farm_fe/src/App.vue
+++ farm_fe/src/App.vue
@@ -1,32 +1,65 @@
 <template>
-  <div id="app" class="Logo">  
-    <button> v-if="isAuth"  Iniciar sesion </button>
-    <button> v-if="isAuth"  Registrarse    </button>
+  <div id="app" class="app">
     
-  
-  <div class="header">
-      <h3>FarmHome</h3>
+    <div class="header">
+    
+      <h1>FarmHome</h1>
+      <nav>
+        <button v-if="is_auth" > Inicio </button>
+        <button v-if="is_auth" > Cuenta </button>
+        <button v-if="is_auth" > Cerrar Sesión </button>
+        <button v-if="!is_auth" v-on:click="loadLogIn" > Iniciar Sesión </button>
+        <button v-if="!is_auth" v-on:click="loadSignUp" > Registrarse </button>
+      </nav>
     </div>
+
+    <div class="main-component">
+      <router-view
+        v-on:completedLogIn="completedLogIn"
+        v-on:completedSignUp="completedSignUp"
+      >
+      </router-view>
+    </div>
+
+    <div class="footer">
+      <h2>Frutasss</h2>
+    </div>
+
   </div>
 </template>
 
 <script>
-export default{
+export default { 
   name: 'App',
 
-  data: function(
-    isAuth = false
-  
-  ){
+  data: function(){
+    return{
+      is_auth: false
+    }
+  },
+
+  components: {
   },
 
   methods:{
-    
+    verifyAuth: function() {
+      if(this.is_auth == false)
+        this.$router.push({name: "logIn"})
+  },
+
+    loadLogIn: function(){
+      this.$router.push({name: "logIn"})
+  },
+    loadSignUp: function(){
+      this.$router.push({name: "signUp"})
+  },
+    completedLogIn: function(data) {},
+    completedSignUp: function(data) {},
   },
   
   created: function(){
-  }
-
+    this.verifyAuth()
+  } 
 }
 </script>
 
@@ -42,9 +75,9 @@ export default{
     height: 10vh;
     min-height: 100px;
 
-    background-color: #46f524 ;
+    background-color: #0b3b02 ;
     color:#E5E7E9;
-    display: flex;
+    display: flex;                                                                                  
     justify-content: space-between;
     align-items: center;
   }

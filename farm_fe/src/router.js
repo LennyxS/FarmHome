497f67e287d1397adff6f5df43be4cbf28ec7abe
LennyxS <lennyxsebastian11@gmail.com>
2021-12-10 00:08:29 -0500

    Agrego lo basico de login y singup

--- farm_fe/src/router.js
+++ farm_fe/src/router.js
@@ -1,30 +1,30 @@
-import { Namespaces }                         from '@vue/compiler-core';
-import { createRouter, createWebHashHistory } from 'vue-router'
-import App                                    from './App.vue'
-import LogIn                                  from './components/LogIn.vue'
-import SignUp                                 from './components/SignUp.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import App from './App.vue';
+
+import LogIn from './components/LogIn.vue'
+import SignUp from './components/SignUp.vue'
+
 
 const routes = [{
-    path: '/',
-    name: 'root',
-    component: App
-  },
-  {
-    path: '/user/logIn',
-    name: 'logIn',
-    component: LogIn
-  },
-  {
-
-    path: '/user/signUp',
-    name: 'signUp',
-    component: SignUp
-  },
+        path: '/',
+        name: 'root',
+        component: App
+    },
+    {
+        path: '/user/logIn',
+        name: "logIn",
+        component: LogIn
+    },
+    {
+        path: '/user/signUp',
+        name: "signUp",
+        component: SignUp
+    }
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
+    history: createWebHistory(),
+    routes,
 });
 
 export default router;
